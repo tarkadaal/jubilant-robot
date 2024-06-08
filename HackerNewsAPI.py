8ec460903e78be93110790c4a28a8887d56a80bf
Dan Jackson <danjacksonis@googlemail.com>
2024-06-08 10:44:47 +0200

    Add functions implementing first two endpoints

--- HackerNewsAPI.py
+++ HackerNewsAPI.py
@@ -1,3 +1,6 @@
+from collections import Counter
+from functools import cache
+from itertools import chain
 import requests
 
 BASE_URL = 'https://hacker-news.firebaseio.com/v0/'
@@ -7,17 +10,15 @@ TOP_STORIES_URL = BASE_URL + 'topstories.json'
 # HackerNews' API refers to comments as kids
 # https://github.com/HackerNews/API?tab=readme-ov-file#items
 COMMENTS = 'kids'
+TEXT = 'text'
 
 def get_top_stories_ids():
   return requests.get(TOP_STORIES_URL).json()
 
+@cache
 def get_item(id):
   string_id = str(id)
   return requests.get(f'{ITEM_URL + string_id}.json').json()
-
-
-def get_first_n_from_sequence(seq, n):
-  return [x for i, x in enumerate(seq) if i < n]
 
 def get_first_n_items_by_id(ids, n):
   first_n_ids = get_first_n_from_sequence(ids, n)
@@ -28,16 +29,27 @@ def get_top_n_stories(n):
   return get_first_n_items_by_id(top_stories_ids, n)
 
 def get_first_n_comments_from_item(item, n):
-  ids = item[COMMENTS]
+  ids = item.get(COMMENTS, [])
   return get_first_n_items_by_id(ids, n)
 
 
-stories = get_top_n_stories(5)
-printable = [str(x) for x in stories] 
-print('\n'.join(printable))
 
-print
+# functions below this comment should be pure
+def get_first_n_from_sequence(seq, n):
+  return [x for i, x in enumerate(seq) if i < n]
 
-printable = [str(x) for x in get_first_n_comments_from_item(stories[1], 50)]
-print('\n'.join(printable))
+def endpoint_1():
+  top_stories_ids = get_top_stories_ids()
+  stories = get_first_n_items_by_id(top_stories_ids, 100)
+  comments = chain.from_iterable(get_first_n_comments_from_item(x, 50) for x in stories)
+  return comments
 
+
+def endpoint_2():
+    top_stories_ids = get_top_stories_ids()
+    stories = get_first_n_items_by_id(top_stories_ids, 30)
+    comments = chain.from_iterable(get_first_n_comments_from_item(x, 100) for x in stories)
+    texts = [x[TEXT] for x in comments if TEXT in x]
+    words = chain.from_iterable(x.split() for x in texts)
+    counted_words = Counter(words)
+    return counted_words.most_common(10)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jubilant-robot
 HackerNews API Aggregator
+
+# Assumptions
+- Jobs counts as stories
+- "10 most used words for the first 100 comments for the top 30 stories" means the most used words across _all_ of those 3000 comments; this endpoint returns 10 words.
+- Endpoint 3 should return the _10_ most used words in all comments; this number is not specified.
