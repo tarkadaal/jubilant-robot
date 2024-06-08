9419640e6b5f67bb443772efc824cefe671ae05c
Dan Jackson <danjacksonis@googlemail.com>
2024-06-08 12:02:04 +0200

    Correctly filter out jobs from the top stories list

--- HackerNewsAPI.py
+++ HackerNewsAPI.py
@@ -11,6 +11,9 @@ TOP_STORIES_URL = BASE_URL + "topstories.json"
 # https://github.com/HackerNews/API?tab=readme-ov-file#items
 COMMENTS = "kids"
 TEXT = "text"
+TYPE = "type"
+
+TYPE_STORY = "story"
 
 
 def get_top_stories_ids():
@@ -30,37 +33,42 @@ def get_first_n_items_by_id(ids, n):
 
 def get_top_n_stories(n):
     top_stories_ids = get_top_stories_ids()
-    return get_first_n_items_by_id(top_stories_ids, n)
+    stories = []
+    while len(stories) < n and len(top_stories_ids) > 0:
+      id = top_stories_ids.pop(0)
+      item = get_item(id)
+      if TYPE in item and item[TYPE] == TYPE_STORY:
+        stories.append(item)
+    return stories
 
 
 def get_first_n_comments_from_item(item, n):
     ids = item.get(COMMENTS, [])
     return get_first_n_items_by_id(ids, n)
 
+
 def get_all_comments_from_item_recursive(item):
     if COMMENTS not in item:
-      return []
+        return []
     else:
-      all_comments = []
-      comments = [get_item(x) for x in item[COMMENTS]]
-      all_comments.extend(comments)
-      for comment in comments:
-        all_comments.extend(get_all_comments_from_item_recursive(comment))
-      return all_comments
+        all_comments = []
+        comments = [get_item(x) for x in item[COMMENTS]]
+        all_comments.extend(comments)
+        for comment in comments:
+            all_comments.extend(get_all_comments_from_item_recursive(comment))
+        return all_comments
 
 
 def endpoint_1():
-    top_stories_ids = get_top_stories_ids()
-    stories = get_first_n_items_by_id(top_stories_ids, 100)
+    stories = get_top_n_stories(100)
     comments = chain.from_iterable(
         get_first_n_comments_from_item(x, 50) for x in stories
     )
-    return comments
+    return list(comments)
 
 
 def endpoint_2():
-    top_stories_ids = get_top_stories_ids()
-    stories = get_first_n_items_by_id(top_stories_ids, 30)
+    stories = get_top_n_stories(30)
     comments = chain.from_iterable(
         get_first_n_comments_from_item(x, 100) for x in stories
     )
@@ -69,19 +77,18 @@ def endpoint_2():
     counted_words = Counter(words)
     return counted_words.most_common(10)
 
+
 def endpoint_3():
-    top_stories_ids = get_top_stories_ids()
-    stories = get_first_n_items_by_id(top_stories_ids, 10)
+    stories = get_top_n_stories(10)
     comments = chain.from_iterable(
-      get_all_comments_from_item_recursive(x) for x in stories
+        get_all_comments_from_item_recursive(x) for x in stories
     )
     texts = [x[TEXT] for x in comments if TEXT in x]
     words = chain.from_iterable(x.split() for x in texts)
     counted_words = Counter(words)
     return counted_words.most_common(10)
 
+
 # functions below this comment should be pure
 def get_first_n_from_sequence(seq, n):
     return [x for i, x in enumerate(seq) if i < n]
-
-
